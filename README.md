d928634d8b4377bd856917c4858bb481e024f672
Vladimir Bogdanov <v.bogdanov@arenadata.io>
2025-03-25 17:12:34 +0300

    Add simple fastapi app

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simple FastAPI app
+This is simple FastAPI application that demonstrates both web pages and API endpoints
+
+## How to use
+- Install dependency:
+```
+python -m pip install -r requirements.txt
+```
+- Run the application using the following command:
+```
+uvicorn app:app --reload
+```
+- The application is available at the following address:
+```
+http://127.0.0.1:8000
+```
---
+++ app.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from api.routes import api_router
+
+app = FastAPI(title="Movies App")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+app.include_router(api_router)
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "title": "Home Page"}
+    )
+
+@app.get("/about/", response_class=HTMLResponse)
+def about(request: Request):
+    return templates.TemplateResponse(
+        "about.html",
+        {
+            "request": request,
+            "title": "About",
+            "developer": "Vladimir Bogdanov",
+            "developer_info": "The newbie python developer with experience in Ansible",
+            "site_info": "This site is a simple FastAPI application showcasing both web pages and API endpoints."
+        }
+    )
